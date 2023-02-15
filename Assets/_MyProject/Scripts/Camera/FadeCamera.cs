15321418ed3d7829afbad2f63ed3339c4312160d
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-15 13:17:14 +0800

    fixing the changes to ensure user can manuvere

--- Assets/_MyProject/Scripts/Camera/FadeCamera.cs
+++ Assets/_MyProject/Scripts/Camera/FadeCamera.cs
@@ -97,7 +97,7 @@ public class FadeCamera : MonoBehaviour
 
 	private void Completete()
     {
-		SceneManager.LoadScene("GameScene 2", LoadSceneMode.Additive);
+		SceneManager.LoadScene("GameScene");
     }
 
 	private void FadeMenu()
