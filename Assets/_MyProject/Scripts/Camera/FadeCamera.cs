2498e13f7f2cbf593f75953bc3344f6fbcd4ecf2
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-15 22:03:02 +0800

    ensure volume is able to work

--- Assets/_MyProject/Scripts/Camera/FadeCamera.cs
+++ Assets/_MyProject/Scripts/Camera/FadeCamera.cs
@@ -97,7 +97,7 @@ public class FadeCamera : MonoBehaviour
 
 	private void Completete()
     {
-		SceneManager.LoadScene("GameScene");
+		SceneManager.LoadScene("GameScene 2");
     }
 
 	private void FadeMenu()
