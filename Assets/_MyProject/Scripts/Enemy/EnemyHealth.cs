eefdc4ce37fc655283520c0adbc3334dc6b82b3e
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-16 15:24:30 +0800

    enemy health script

---
+++ Assets/_MyProject/Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+//---------------------------------------------------------------------------------
+// Author		: Gabriel Loke
+// Date  		: 10/02/23
+// Description	: management of enemy health
+//---------------------------------------------------------------------------------
+public class EnemyHealth : MonoBehaviour
+{
+    [Header("Health Properties")]
+    [SerializeField] private int maxHealth = 100; //How much health this enemy has
+    private int currentHealth; //Current health amount of enemy
+    //This method is called whenever the enemy is damaged by an attack
+    public void TakeDamage(int amount)
+    {
+        if (currentHealth <= 0)
+        {
+        //If the enemy is already defeated leave using return else can destroy
+        Destroy(this.gameObject);
+        }
+        //Reduce the current health by the amount of damage
+        currentHealth -= amount;
+    }
+
+    void OnEnable ()
+    {
+        currentHealth = maxHealth;
+    }
+}
