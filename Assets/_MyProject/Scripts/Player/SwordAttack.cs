0f2ca5f18405c1947e0dddea938c347160de2c76
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-16 15:28:37 +0800

    creation of sword attack script

---
+++ Assets/_MyProject/Scripts/Player/SwordAttack.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+//---------------------------------------------------------------------------------
+// Author		: Gabriel Loke
+// Date  		: 10/02/23
+// Description	: sword damage script
+//---------------------------------------------------------------------------------
+public class SwordAttack : MonoBehaviour
+{
+    [SerializeField] private int DealDamageAmount = 10;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
+            enemyHealth.TakeDamage(DealDamageAmount);
+            //Destroy(other.gameObject);
+        }
+    }
+}
