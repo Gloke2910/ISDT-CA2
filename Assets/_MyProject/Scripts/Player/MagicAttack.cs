54cbbc54e55cabfe53cf07ee1ebe69c98a906077
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-17 19:05:29 +0800

    magic attack script

---
+++ Assets/_MyProject/Scripts/Player/MagicAttack.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+//---------------------------------------------------------------------------------
+// Author		: Gabriel Loke
+// Date  		: 15/2/23
+// Description	: Magic projectile attack 
+//---------------------------------------------------------------------------------
+
+public class MagicAttack : MonoBehaviour
+{
+    [Header ("Fireball Properties")]
+    public GameObject pf_FireBall;
+    [SerializeField] private GameObject fireBallPt;
+    [SerializeField] private float fireBallSpeed = 200f;
+    [SerializeField] private float fireBallLife = 2f;
+
+    public void FireBallLaunch()
+    {
+        // Instantiate a pf_FireBall at fireBallPt position and no rotation
+        // Call it with a grab interactable activate event 
+        GameObject tmpFireBall;
+        tmpFireBall = Instantiate(pf_FireBall, fireBallPt.transform.position, Quaternion.identity);
+        tmpFireBall.GetComponent<Rigidbody>().AddForce(fireBallPt.transform.forward * fireBallSpeed);
+
+        Destroy(tmpFireBall.transform.gameObject, fireBallLife); // Destroys the fireball
+    }
+}
