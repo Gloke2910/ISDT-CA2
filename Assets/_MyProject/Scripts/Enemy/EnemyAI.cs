68dd3c7f237c15cce08636b28453aa7c407487d7
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-18 22:06:38 +0800

    Enemy AI Script

--- Assets/_MyProject/Scripts/Enemy/EnemyAI.cs
+++ Assets/_MyProject/Scripts/Enemy/EnemyAI.cs
@@ -4,98 +4,23 @@ using UnityEngine.AI;
 //---------------------------------------------------------------------------------
 // Author		: Gabriel Loke
 // Date  		: 10/02/23
-// Description	: management of enemy health
+// Description	: Enemy AI
 //---------------------------------------------------------------------------------
 
 
-/*public class EnemyAI : MonoBehaviour
+public class EnemyAI : MonoBehaviour
 {
     public NavMeshAgent agent;
 
     public Transform player;
 
-    public LayerMask whatIsGround, WhatIsPlayer;
-
-    // Patrolling
-    public Vector3 walkPoint;
-    bool walkPointSet;
-    public float walkPointRange;
-
-    // Attacking 
-    public float timeBetweenAttacks;
-    bool alreadyAttacked;
-
-    // States 
-    public float sightRange, attackRange;
-    public bool playerInSightRange, playerInAttackRange;
-
-    private void Awake()
+    private void Start()
     {
-        player = GameObject.Find("Main Camera").transform;
         agent = GetComponent<NavMeshAgent>();
     }
 
     private void Update()
     {
-        // Checks for player
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, WhatIsPlayer);
-        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, WhatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patrolling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+        agent.SetDestination(player.position);
     }
-
-    private void Patrolling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.setDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //walkpoint reached 
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-    }
-
-    private void SearchWalkPoint()
-    {
-        // Calculate random point in range
-        float randomZ = Random.Range(-walkPointRange, walkPointRange);
-        float randomX = Random.Range(-walkPointRange, walkPointRange);
-
-        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
-
-        // Checks if on ground or not
-        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
-            walkPointSet = true;
-    }
-
-    private void ChasePlayer()
-    {
-        agent.setDestination(player.position);
-    }
-    
-    private void AttackPlayer()
-    {
-        // Make sure the enemy doesn't move 
-        agent.setDestination(transform.position);
-
-        transform.LookAt(player);
-
-        if (!alreadyAttacked)
-        {
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
-        }
-    }
-
-    private void ResetAttack()
-    {
-        alreadyAttacked = false;
-    }
-
 }
-*/
