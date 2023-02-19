b8e653ee178fa66b6d7a87676643cd05b320a6c3
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-19 21:40:57 +0800

    Final Menu fixes

--- Assets/_MyProject/Scripts/Manager/GoMenuSword.cs
+++ Assets/_MyProject/Scripts/Manager/GoMenuSword.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class GoMenu : MonoBehaviour
+public class GoMenuSword : MonoBehaviour
 {
     public GameObject Menu; 
     
@@ -11,7 +11,7 @@ public class GoMenu : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A)) {
             Debug.Log("Hello");
-            SceneManager.LoadScene("MenuSceneSpells");
+            SceneManager.LoadScene("MenuSceneSwords");
         }
     }
 }
