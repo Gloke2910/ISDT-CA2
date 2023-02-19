087ba214f4c615f1ad35955e7e75dcd1a12322ec
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-19 18:38:54 +0800

    enemy health changes

--- Assets/_MyProject/Scripts/Enemy/EnemyHealth.cs
+++ Assets/_MyProject/Scripts/Enemy/EnemyHealth.cs
@@ -11,7 +11,7 @@ public class EnemyHealth : MonoBehaviour
     [Header("Health Properties")]
     [SerializeField] private int maxHealth = 100; //How much health this enemy has
     private int currentHealth; //Current health amount of enemy
-    public int Destroy;
+    //public int Destroy;
     //This method is called whenever the enemy is damaged by an attack
     public void TakeDamage(int amount)
     {
@@ -19,7 +19,7 @@ public class EnemyHealth : MonoBehaviour
         {
         //If the enemy is already defeated leave using return else can destroy
         Destroy(this.gameObject);
-        Destroy += 1;
+        //Destroy += 1;
         }
         //Reduce the current health by the amount of damage
         currentHealth -= amount;
