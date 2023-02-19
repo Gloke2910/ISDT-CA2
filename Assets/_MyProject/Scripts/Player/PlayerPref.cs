5bb1b6ef30203293d6c578f2787d4ca71c742111
Gloke2910 <gabrielloke2042@gmail.com>
2023-02-19 19:44:39 +0800

    player pref creation

---
+++ Assets/_MyProject/Scripts/Player/PlayerPref.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//---------------------------------------------------------------------------------
+// Author		: Gabriel Loke
+// Date  		: 18/02/23
+// Description	: saves player location
+//---------------------------------------------------------------------------------
+
+/*
+[CreateAssetMenu(fileName: "Player Data", order = 1)]
+public class PlayerPref : ScriptableObject
+{
+    [Header("Location")];
+
+    [SerializeField];
+    int location; 
+}
+*/
